ec83b63caa294fa1e0c00c34b6baf8b780a9eb6f
Jerry Wilson <mrjjwilson@gmail.com>
2021-04-26 21:57:57 +0800

    initial prismic starter commit

---
+++ prismic-configuration.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The repo name for your Prismic repository (found in the url of your repo: https://fitwithjerry.prismic.io/)
+  prismicRepo: 'fitwithjerry',
+
+  // Release ID to build (requires access)
+  releaseID: '',
+
+  // access token to access the release
+  accessToken: '',
+}
